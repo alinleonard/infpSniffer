f719cec5d040b1752ba35cfdff74ebff333b0285
alinleonard <balinleonard@gmail.com>
2015-06-05 00:44:52 +0300

    clean functions

--- earthquake.py
+++ earthquake.py
@@ -3,45 +3,74 @@ import json
 import os.path
 import time
 import datetime
+import os
+
+def main():
+	print "INFP.ro Sniffer"
+
+	# Retry to get data from infp for 5 time , sleep 30 seconds in between
+	for x in range(0, 5):
+		try:
+			url = urllib2.urlopen('http://www.infp.ro/data/webSeismicity.json')
+			error_fetch = None
+		except urllib2.HTTPError, e:
+			print(e.code)
+			error_fetch = e.code
+		except urllib2.URLError, e:
+			print(e.args)
+			error_fetch = e.code
+		if error_fetch:
+			from time import sleep
+			sleep(30)
+			if x == 5: # If this is the last pass here , and we did try 5 times exit program
+				exit()
+		else:
+			break
+
+	j_obj = json.load(url) #Page loaded fine so we can load the json
+	
+	magnitude = j_obj['local'][0]['magnitude'] 
+	region =  j_obj['local'][0]['region']
+	depth = j_obj['local'][0]['depth']
+	created_at = j_obj['local'][0]['dt_rom']
+
+	# if we don't have magnitude to work with in json then it means we did not load corectly , we exit program
+	if not magnitude:
+		print "NO MAGNITUDE TO READ FROM"
+		exit()
+	
+	if os.path.exists('logs.json') and magnitude >= 4:
+		with open('logs.json') as data_file:
+			j_obj_file = json.load(data_file)
+		if j_obj_file['local'][0]['created_at']: #if we have data to work with ( if file is not empty )
+			if j_obj_file['local'][0]['created_at']	!= created_at: # so we don't save it multiple times
+				print "CUTREMUR (new entry)"
+				logCutremure(magnitude, region, depth, created_at)
+		else: # there is no data to work with so we add now
+			print "CUTREMUR (no data , new entry)"
+			logCutremure(magnitude, region, depth, created_at)
+	else: # there is no file
+		if magnitude >= 4:
+			with open('logs.json','w+') as logFile:
+				json.dump({'magnitude':magnitude, 'region': region, 'depth':depth, 'created_at':created_at}, logFile, indent=4)
+			print "CUTREMUR (no file , new file and data)"
+
+	os.system('cls' if os.name == 'nt' else 'clear') # clear screen
+
+	print "Ultimul cutremur: \n"
+	print "%s - %s - %s km - %s \n" % (magnitude, region, depth, created_at)
+
+	return
 
 def logCutremure(m, r, d, c):
-	fisier =  open('logs.json','a+')
-	json.dump({'magnitude':m, 'region': r, 'depth':d, 'created_at':c}, fisier, indent=4)
+	with open('logs.json','a+') as logFile: #use of with open to open then close imediatly
+		json.dump({'magnitude':m, 'region': r, 'depth':d, 'created_at':c}, logFile, indent=4)
 	print "\t log saved"	
 	return
+
 try:
-	print "INFP.ro Sniffer"
-	print datetime.datetime.now()
-	while True:
-		url = urllib2.urlopen('http://www.infp.ro/data/webSeismicity.json')
-		j_obj = json.load(url)
-	
-		magnitude = j_obj['local'][0]['magnitude'] 
-		region =  j_obj['local'][0]['region']
-		depth = j_obj['local'][0]['depth']
-		created_at = j_obj['local'][0]['dt_rom']
-	
-		#print "Ultimul cutremur: \n"
-		#print "%s - %s - %s km - %s \n" % (magnitude, region, depth, created_at)
-
-
-		if os.path.exists('json-dump.json'):
-			#print "\n\t file exists..."
-			with open('json-dump.json') as data_file:
-				j_obj_old = json.load(data_file)
-			#print "\t ultimul cutremur salvat: %s - %s Mag" % (j_obj_old['local'][0]['dt_rom'],j_obj_old['local'][0]['magnitude'])
-			if j_obj_old['local'][0]['dt_rom'] != created_at and magnitude >= 4:
-				print "\nCUTREMUR (updated)" 
-				file = open('json-dump.json','w+')
-				json.dump(j_obj, file)
-				logCutremure(magnitude, region, depth, created_at)
-		if not os.path.exists('json-dump.json') and magnitude >= 4:
-			print "CUTREMUR (saved)"
-			file = open('json-dump.json','w+')
-			json.dump(j_obj, file)
-			logCutremure(magnitude, region, depth, created_at)
-		time.sleep( 1 )
-		#print "\n\n end"
+	print datetime.datetime.now() #  Print the time when it started running
+	main()
 except KeyboardInterrupt:
-	print datetime.datetime.now()
+	print datetime.datetime.now() # Print the time on exit
 	pass
